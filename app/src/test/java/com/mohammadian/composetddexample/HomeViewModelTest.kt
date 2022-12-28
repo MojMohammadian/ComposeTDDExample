bba33e27e80bca15d70369460758d5a8c7191c26
MojMohammadian <115070390+MojMohammadian@users.noreply.github.com>
2022-12-28 12:51:50 +0330

    Add predicted Tests to complete them later

---
+++ app/src/test/java/com/mohammadian/composetddexample/HomeViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mohammadian.composetddexample
+
+import org.junit.Test
+
+class HomeViewModelTest {
+    private lateinit var testObject: HomeViewModel
+
+    @Test
+    fun `on home view model init validate state default values`() {
+    }
+
+    @Test
+    fun `on home view model init get posts returns success with posts list data`() {
+    }
+
+    @Test
+    fun `on home view model init get posts returns error with socket connection exception`() {
+    }
+
+    @Test
+    fun `on home view model init get posts returns error with http exception`() {
+    }
+
+    @Test
+    fun `on receiving error retry get posts returns success with posts list data`() {
+    }
+}
